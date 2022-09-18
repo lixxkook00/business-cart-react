c6cdaae0d13fab1afb2476ce1cd4b744b033299f
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-09-19 01:23:45 +0700

    update

--- src/components/DemoCart/HomeHeader/index.jsx
+++ src/components/DemoCart/HomeHeader/index.jsx
@@ -1,21 +1,85 @@
 import React from 'react'
 import './HomeHeader.scss'
 
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+
 export default function HomeHeader() {
-  return (
-    <div className="header">
 
-        <div className="header-language">
-            <img src="https://cdn-icons-png.flaticon.com/512/1377/1377975.png" alt="" />
-        </div>
+    const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
 
-        <div className="header-name">
-            Core Power
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <div className="cart">
+                <div className="cart-title">
+                    YOUR CART
+                </div>
+
+                <div className="cart-content">
+                    <div className="cart-item">
+
+                    </div>
+                </div>
+
+                <div className="cart-total">
+                    <div className="cart-total-content">
+                        <span>Total</span>
+                        <h5 className="value">
+                            $0.00
+                        </h5>
+                    </div>
+                    <div className="primary-button">
+                        Order now
+                    </div>
+                </div>
+            </div>
+        </Box>
+    );
+
+    return (
+        <div className="header">
+
+            <div className="header-language">
+                <img src="https://cdn-icons-png.flaticon.com/512/1377/1377975.png" alt="" />
+            </div>
+
+            <div className="header-name">
+                Core Power
+            </div>
+            
+            <div className="header-cart" onClick={toggleDrawer('right', true)}>
+                <i className="fa-solid fa-cart-shopping"></i>
+
+                <div className="header-cart-noti">
+                    0
+                </div>
+            </div>
+
+            <Drawer
+                anchor={'right'}
+                open={state['right']}
+                onClose={toggleDrawer('right', false)}
+            >
+                {list('right')}
+            </Drawer>
         </div>
-        
-        <div className="header-cart">
-            <i className="fa-solid fa-cart-shopping"></i>
-        </div>
-    </div>
-  )
+    )
 }
